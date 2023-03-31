71203e08933a67a9b6dd64af80d8543c22bf55dd
github <mmoloshova@gmail.com>
2023-03-31 20:48:38 +0300

    first commit

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -1,7 +1,27 @@
 package peaksoft;
 
+import peaksoft.dao.UserDaoJdbcImpl;
+import peaksoft.model.User;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+//        Util.connection().close();
+        UserDaoJdbcImpl userDaoJdbc=new UserDaoJdbcImpl();
+//        userDaoJdbc.createUsersTable();
+        userDaoJdbc.saveUser("Kanat","Kubanov",(byte) 23);
+        userDaoJdbc.saveUser("Malika","Moloshova",(byte) 17);
+        userDaoJdbc.saveUser("Nurisa","Jarmamat kyzy",(byte) 20);
+        userDaoJdbc.saveUser("Bakyt","Latibov",(byte) 22);
+        userDaoJdbc.saveUser("Uson","Kamylov",(byte) 22);
+        userDaoJdbc.saveUser("Asan","Tairov",(byte) 23);
+//        userDaoJdbc.removeUserById(1);
+//        userDaoJdbc.dropUsersTable();
+//        for(User user: userDaoJdbc.getAllUsers()){
+//            System.out.println(user);
+//        }
+//        userDaoJdbc.cleanUsersTable();
+
     }
 }
--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
@@ -1,5 +1,23 @@
 package peaksoft.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL="jdbc:postgresql://localhost:5432/postgres";
+    private static final String  USER_NAME="postgres";
+    private static final String  PASSWORD="moloshova06";
+
+    public static Connection connection(){
+        Connection connection=null;
+        try{
+            connection= DriverManager.getConnection(URL,USER_NAME,PASSWORD);
+            System.out.println("Connected to the postgresql successfully");
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return connection;
+    }
 }
